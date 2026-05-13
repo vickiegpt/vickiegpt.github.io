32c38664d0fc0e101722b4223b4c32c2418a50e3
vickieGPT <victoryang00@ucsc.edu>
2026-05-13 00:09:22 +0000

    update

--- cxl2/cxl-module.js
+++ cxl2/cxl-module.js
@@ -184,7 +184,7 @@ const CXL_WEB_CONFIG = (() => {
             thread: tcgThread,
             tbSize
         },
-        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260512-relfix1',
+        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260512-o3clean-test',
         assetBase: qemuCore === 'fpcast' ? '/cxl2/images/alpine-x86_64-fpcast/' : '/cxl2/images/alpine-x86_64/',
         image,
         network: {
