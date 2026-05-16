b55932ab6e0a717f0c496e41f9cb038cfcb7234d
vickieGPT <victoryang00@ucsc.edu>
2026-05-16 06:12:17 +0000

    Use RTC UIP fixed QEMU wasm

--- cxl2/cxl-module.js
+++ cxl2/cxl-module.js
@@ -282,7 +282,7 @@ const CXL_WEB_CONFIG = (() => {
             thread: tcgThread,
             tbSize
         },
-        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260516-docker-crssort',
+        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260516-rtc-uip',
         assetBase: qemuCore === 'fpcast' ? '/cxl2/images/alpine-x86_64-fpcast/' : '/cxl2/images/alpine-x86_64/',
         image,
         network: {
