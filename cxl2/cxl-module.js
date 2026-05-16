755773b969c5e3f27c64abd1b22bd1d792879b15
vickieGPT <victoryang00@ucsc.edu>
2026-05-16 03:42:40 +0000

    update

--- cxl2/cxl-module.js
+++ cxl2/cxl-module.js
@@ -252,7 +252,7 @@ const CXL_WEB_CONFIG = (() => {
             thread: tcgThread,
             tbSize
         },
-        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260516-docker-dyncall',
+        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260516-docker-dyncalls',
         assetBase: qemuCore === 'fpcast' ? '/cxl2/images/alpine-x86_64-fpcast/' : '/cxl2/images/alpine-x86_64/',
         image,
         network: {
