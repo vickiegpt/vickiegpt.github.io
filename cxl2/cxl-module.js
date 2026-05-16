9434002fcfcb394da1e308ac27bace8896f2ab17
vickieGPT <victoryang00@ucsc.edu>
2026-05-16 04:10:15 +0000

    update

--- cxl2/cxl-module.js
+++ cxl2/cxl-module.js
@@ -252,7 +252,7 @@ const CXL_WEB_CONFIG = (() => {
             thread: tcgThread,
             tbSize
         },
-        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260516-docker-propsend',
+        assetVersion: qemuCore === 'fpcast' ? '20260512-numfix' : '20260516-docker-crssort',
         assetBase: qemuCore === 'fpcast' ? '/cxl2/images/alpine-x86_64-fpcast/' : '/cxl2/images/alpine-x86_64/',
         image,
         network: {
